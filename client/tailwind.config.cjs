c5a76f5bbd82734f78718ba3356852c9e63992f6
Sattwik <hellosattwik@gmail.com>
2022-10-01 23:05:38 +0530

    added daisyui && reomved material tailwind

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require('@material-tailwind/react/utils/withMT');
-
-module.exports = withMT({
+module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-});
+  plugins: [require('daisyui')],
+};
