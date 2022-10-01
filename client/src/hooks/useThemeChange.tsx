57c5a8074bddbbf2244bfacaa83b03958790825b
Sattwik <hellosattwik@gmail.com>
2022-10-02 00:11:38 +0530

    fixed useThemeChange hook!

--- client/src/hooks/useThemeChange.tsx
+++ client/src/hooks/useThemeChange.tsx
@@ -1,10 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const getCurrentTheme = () => {
+  const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  if (isDarkTheme) return 'dark';
+
+  return 'light';
+};
 
 export const useThemeChange = () => {
+  const [theme, setTheme] = useState(getCurrentTheme());
+
   useEffect(() => {
     const html = document.querySelector('html');
-    html?.setAttribute('data-theme', 'cyberpunk');
-  }, []);
+    html?.setAttribute('data-theme', theme);
+  }, [theme]);
 
-  return;
+  return setTheme;
 };
