31dfcd728e301fdd6d68bcba680d93a3b34d9486
Sattwik <hellosattwik@gmail.com>
2022-10-01 23:44:27 +0530

    tailwind config fixed!

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
